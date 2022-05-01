560aaca4aec2838c1cf99d3c24e4d0533b0019fb
Your Name <you@example.com>
2022-05-01 13:25:34 +0530

    Added:welcome message

---
+++ src/com/empWage/EmployeeWage.java
@@ -0,0 +1,13 @@
+package com.empWage;
+
+import java.util.Random;
+
+public class EmployeeWage {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to empWage");
+		
+	
+	}
+
+}
