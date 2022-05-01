7f60ae7d87fb31911015f98003c30551836a7e64
Your Name <you@example.com>
2022-05-01 13:30:15 +0530

    Added:check emp present or abscent

--- src/com/empWage/EmployeeWage.java
+++ src/com/empWage/EmployeeWage.java
@@ -6,7 +6,15 @@ public class EmployeeWage {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to empWage");
-		
+		Random ran = new Random();
+		int present = 1;
+		int empCheck = ran.nextInt(2);
+		if (empCheck == present) {
+			System.out.println("Employee is Present");
+		}
+		else {
+			System.out.println("Employee is Absent");
+		}
 	
 	}
 
